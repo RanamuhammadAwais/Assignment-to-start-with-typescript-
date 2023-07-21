37d6ed7ea8b0094ba8e5af6f7b7c9c53fe6a1840
RanamuhammadAwais <ranamawais92@gmail.com>
2023-07-21 17:04:40 +0500

    upto 6 questions done

---
+++ sn.js
@@ -0,0 +1,24 @@
+"use strict";
+let a = "Ahmed";
+console.log("Hello", a, "would you like to learn some Python today?");
+/*Name Cases: Store a person’s name in a variable,
+and then print that person’s name in lowercase, uppercase, and titlecase
+*/
+let b = "eric";
+console.log(b.toLowerCase());
+console.log(b.toUpperCase());
+/*Famous Quote: Find a quote from a famous person you admire.
+Print the quote and the name of its author */
+console.log('J. R. R. Tolkein said, "Not all those who wander are lost."');
+/*Famous Quote 2: Repeat Exercise 4, but this time store the famous
+person’s name in a variable called famous_person. Then compose your
+message and store it in a new variable called message. Print your message.
+*/
+let famous_person = "J. R. R. Tolkein said,";
+let message = '"Not all those who wander are lost."';
+console.log(famous_person + message);
+// Stripping Names: Store a person’s name, and include
+//  some whitespace characters at the beginning and end 
+//  of the name.
+var ak = "\tRana Awais'\n";
+console.log(ak);
